fac509d10c9df163fe0659077d7947336f1a087c
Charlie Gao <chg774@g.harvard.edu>
2018-02-04 11:25:41 -0500

    rm unused js

--- index.php
+++ index.php
@@ -9,8 +9,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css' type='text/css'>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" type='text/css'>
-
     <link rel="stylesheet" href="css/main.css" type='text/css'>
 </head>
 <body>
@@ -75,8 +73,5 @@
         </blockquote>
 
     </div>
-
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </body>
 </html>
